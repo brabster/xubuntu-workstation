6f4a267d45113bfddfbc09aee1db1c5d08601665
Paul Brabban <paul.brabban@gmail.com>
2022-07-02 20:46:54 +0100

    Update to work better with insta-timeout sude

--- setup.sh
+++ setup.sh
@@ -2,15 +2,16 @@
 
 set -eu
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git ansible
+echo "=== Updating packages and installing git & ansible (sudo required)"
+sudo sh -c 'apt-get update && apt-get upgrade && apt-get install git ansible'
 
+echo "=== Cloning repo"
 mkdir -p ~/projects/brabster
 cd ~/projects/brabster
 git clone https://github.com/brabster/xubuntu-workstation.git
-
 cd ~/projects/brabster/xubuntu-workstation
+
+echo "=== Running playbook (sudo required)"
 ansible-playbook -K workstation.yml
 git remote rm origin
 git remote add origin git@github.com:brabster/xubuntu-workstation.git
