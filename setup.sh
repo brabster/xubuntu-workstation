ea43f43510498ff3603a5a8c70b503442d9cc23b
Paul Brabban <paul.brabban@gmail.com>
2023-07-05 22:04:47 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,16 +2,14 @@
 
 set -eu
 
-echo "=== Updating packages and installing git & ansible (sudo required)"
+echo "=== setup.sh must be run as root."
+
+echo "=== Updating packages and installing git & ansible"
 sudo sh -c 'apt-get update && apt-get upgrade && apt-get install git ansible'
 
-echo "=== Cloning repo"
-mkdir -p ~/projects/brabster
-cd ~/projects/brabster
+echo "=== Cloning repo into $(pwd)"
 git clone https://github.com/brabster/xubuntu-workstation.git
-cd ~/projects/brabster/xubuntu-workstation
 
-echo "=== Running playbook (sudo required)"
-ansible-playbook -K workstation.yml
-git remote rm origin
-git remote add origin git@github.com:brabster/xubuntu-workstation.git
+echo "=== Update undefined vars.yml"
+echo "=== Then run ansible-playbook workstation.yml"
+
