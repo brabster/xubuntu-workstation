b9bd850e3241de657806b34c5f3e2c8701cce7b9
Paul Brabban <paul.brabban@gmail.com>
2024-02-24 23:36:48 +0000

    update bootstrap for second-usb approach

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,8 @@ echo "=== setup.sh must be run as root."
 echo "=== Updating packages and installing git & ansible"
 sudo sh -c 'apt-get update && apt-get upgrade && apt-get install git ansible'
 
-echo "=== Cloning repo into $(pwd)"
-git clone https://github.com/brabster/xubuntu-workstation.git
+echo "=== Running playbook"
+ansible-playbook workstation.yml
 
-echo "=== cd into xubuntu-workstation.."
-echo "=== Create gitignored .vars.yml with undefined form vars.yml (or pass --extra-vars arg on next instruction)"
-echo "=== Then run ansible-playbook workstation.yml"
-echo "=== Finally, log out and in again, no need to reboot."
+echo "=== Log out and in again, no need to reboot."
 
