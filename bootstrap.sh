9e1d13316b65512b00d89458158df425ba3a4201
Paul Brabban <paul.brabban@gmail.com>
2024-02-26 19:44:48 +0000

    set bootstrap script to execute without intevention

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ set -eu
 echo "=== setup.sh must be run as root."
 
 echo "=== Updating packages and installing git & ansible"
-sudo sh -c 'apt-get update && apt-get upgrade && apt-get install git ansible'
+sudo sh -c 'apt-get -y update && apt-get -y upgrade && apt-get -y install git ansible'
 
 echo "=== Running playbook"
 ansible-playbook workstation.yml
