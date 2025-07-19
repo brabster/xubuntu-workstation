f1c6922b4110d179d619bbd31afe34c0df0d0c0a
Paul Brabban <paul.brabban@gmail.com>
2025-07-19 22:47:28 +0000

    fix another issue with the script, no AI juice left

--- roles/clamav/files/clamav-quarantine.sh
+++ roles/clamav/files/clamav-quarantine.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-# Quarantine directory
-QUARANTINE_DIR="$HOME/Downloads/.quarantine"
+# This script is run by clamd as the 'clamav' user.
+# The full path to the infected file is passed as the first argument.
+INFECTED_FILE="$1"
 
-# Ensure the quarantine directory exists
+# The quarantine directory must have a hardcoded path or be passed as an argument.
+# It cannot rely on the user's HOME environment variable.
+QUARANTINE_DIR="/home/tester/Downloads/.quarantine"
+
+# Ensure the quarantine directory exists.
 mkdir -p "$QUARANTINE_DIR"
 
-# Move the infected file to the quarantine directory
-mv "$1" "$QUARANTINE_DIR/"
+# Move the infected file into it.
+mv "$INFECTED_FILE" "$QUARANTINE_DIR/"
 
 # Send a desktop notification
 su -c "notify-send 'Virus Detected' 'The file $(basename "$1") has been quarantined.'" -l "$USER"
+
+exit 0
